92d27a8893d8b3f56db05494143a9d5161d7f9d9
Adrian Kalinin <adriankalinin@protonmail.com>
2021-08-21 02:51:55 +0300

    add settings

---
+++ settings.py
@@ -0,0 +1,29 @@
+from configparser import ConfigParser
+
+
+config = ConfigParser()
+config.read('config.ini')
+
+
+# bot section
+BOT_TOKEN = config.get('bot', 'token')
+ADMINS = tuple(map(int, config.get('bot', 'admins').split(', ')))
+DEVELOPER = config.getint('bot', 'developer')
+CHANNEL = config.get('bot', 'channel')
+
+# database section
+DATABASE_PATH = config.get('database', 'sqlite')
+
+# server section
+LISTEN = config.get('server', 'listen')
+HOST = config.get('server', 'host')
+PORT = config.getint('server', 'port')
+
+# webhook section
+WEBHOOK_URL = config.get('webhook', 'webhook_url')
+KEY_PATH = config.get('webhook', 'key_path')
+CERT_PATH = config.get('webhook', 'cert_path')
+
+# instagram section
+INSTAGRAM_USERNAME = config.get('instagram', 'username')
+INSTAGRAM_PASSWORD = config.get('instagram', 'password')
